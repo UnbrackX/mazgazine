74c6f4953e62009805ccd5da8136799407a26798
UnbrackX <kodirovmy122109@gmail.com>
2026-01-05 15:03:36 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <h1>Hello world</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis magni voluptas ullam quos nesciunt? Reprehenderit ab nihil maxime nobis veniam?</p>
     </>
   )
 }
