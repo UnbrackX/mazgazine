11daaea47d757eb0f1aa7c8e0c58b5a4523ec8cf
khalimovdev1 <khalimovdev66@gmail.com>
2026-01-05 15:15:21 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>Hello world</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis magni voluptas ullam quos nesciunt? Reprehenderit ab nihil maxime nobis veniam?</p>
+     
     </>
   )
 }
