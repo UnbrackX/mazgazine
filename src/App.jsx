ac33d1d2df4e52dbe96262925ad6d1573b75b82f
UnbrackX <kodirovmy122109@gmail.com>
2026-01-05 15:16:42 +0500

    My changes before merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>Hello world</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis magni voluptas ullam quos nesciunt? Reprehenderit ab nihil maxime nobis veniam?</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis magni
+        voluptas ullam quos nesciunt? Reprehenderit ab nihil maxime nobis
+        veniam?
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
